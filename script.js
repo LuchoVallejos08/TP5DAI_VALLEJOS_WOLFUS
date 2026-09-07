fbccceb477c64d717e6ebb9b38c9675099f7381a
MATÍAS WOLFUS <49194355@ort.edu.ar>
2026-09-07 08:20:00 -0300

    .

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 async function obtenerDatos() {
   try {
-    document.getElementById("resultado").style.display = "block";   
+    document.getElementById("error").style.display = "none";
     const input = document.getElementById("ingreso");
     const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + input.value);
+    if (!response.ok) {
+      throw new Error("no se encontro el pokemon");
+    }
     const data = await response.json();
-        document.getElementById("pokeImagen").src = data.sprites.front_default;
-        document.getElementById("pokeNombre").textContent = data.name;
-        const tipos = []; 
-        data.types.forEach(element => {
-          tipos.push(element.type.name);
-        });
-        document.getElementById("pokeTipos").textContent = tipos;
-        document.getElementById("pokePeso").textContent = `Peso: ${data.weight} kg`;
+    document.getElementById("resultado").style.display = "block";
+    document.getElementById("pokeImagen").src = data.sprites.front_default;
+    document.getElementById("pokeNombre").textContent = data.name;
+    const tipos = [];
+    data.types.forEach(element => {
+      tipos.push(element.type.name);
+    });
+    document.getElementById("pokeTipos").textContent = tipos;
+    document.getElementById("pokePeso").textContent = `Peso: ${data.weight} kg`;
 
   } catch (error) {
-    const errorM = document.getElementById("error");
-    errorM = "no se encontro el pokemon"
-    }
+    document.getElementById("resultado").style.display = "none";
+    document.getElementById("error").style.display = "block";
+  }
 }
 
 function buscarPokemon() {
